f4bc30b7cbc7f30b939a8d67b35944e94b301ad8
galina kononovich <galina.kononovich@bgaming.com>
2022-12-30 23:58:41 +0400

    Incubator: hw-5, add 404 error

--- src/s2-homeworks/hw05/sidebar/Sidebar.tsx
+++ src/s2-homeworks/hw05/sidebar/Sidebar.tsx
@@ -12,7 +12,7 @@ type PropsType = {
 export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
     const sidebarClass = s.sidebar
         + (open ? ' ' + s.open : '')
-        console.log(sidebarClass)
+
     return (
         <>
             {/*затемнение справа от открытого меню*/}
@@ -34,7 +34,7 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         id={'hw5-pre-junior-link'}
                         to={PATH.PRE_JUNIOR}
                         onClick={handleClose}
-                        className={PATH.PRE_JUNIOR? s.active : ''} // делает студент
+                        className={({ isActive }) => (isActive ? 'active' : 'inactive')} // делает студент
                     >
                         Pre-junior
                     </NavLink>
@@ -42,7 +42,7 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         id={'hw5-junior-link'}
                         to={PATH.JUNIOR}
                         onClick={handleClose}
-                        className={PATH.JUNIOR? s.active : ''} // делает студент
+                        className={({ isActive }) => (isActive ? 'active' : 'inactive')} // делает студент
                     >
                         Junior
                     </NavLink>
@@ -50,7 +50,7 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         id={'hw5-junior-plus-link'}
                         to={PATH.JUNIOR_PLUS}
                         onClick={handleClose}
-                        className={PATH.JUNIOR_PLUS? s.active : ''} // делает студент
+                        className={({ isActive }) => (isActive ? 'active' : 'inactive')} // делает студент
                     >
                         Junior Plus
                     </NavLink>
