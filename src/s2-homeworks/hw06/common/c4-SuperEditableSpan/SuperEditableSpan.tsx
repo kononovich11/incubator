ea98df7a2843002ab190333522a539980088ee90
galina kononovich <galina.kononovich@bgaming.com>
2022-12-31 19:52:15 +0400

    Incubator: hw-6, work with localStorage

--- src/s2-homeworks/hw06/common/c4-SuperEditableSpan/SuperEditableSpan.tsx
+++ src/s2-homeworks/hw06/common/c4-SuperEditableSpan/SuperEditableSpan.tsx
@@ -41,20 +41,17 @@ const SuperEditableSpan: React.FC<SuperEditableSpanType> = (
     spanProps || {}
 
     const onEnterCallback = () => {
-        // выключить editMode при нажатии Enter // делают студенты
-
+        setEditMode(false); // выключить editMode при нажатии Enter // делают студенты
         onEnter?.()
     }
     const onBlurCallback = (e: React.FocusEvent<HTMLInputElement>) => {
-        // выключить editMode при нажатии за пределами инпута // делают студенты
-
+        setEditMode(false); // выключить editMode при нажатии за пределами инпута // делают студенты
         onBlur?.(e)
     }
     const onDoubleClickCallBack = (
         e: React.MouseEvent<HTMLSpanElement, MouseEvent>
     ) => {
-        // включить editMode при двойном клике // делают студенты
-
+        setEditMode(true); // включить editMode при двойном клике // делают студенты
         onDoubleClick?.(e)
     }
 
